c1457ed68fd239caa1ad72d2073d06a454bc03ea
Ivan Zichtl <izichtl@gmail.com>
2025-07-14 14:20:21 -0300

    main(main): updated tooltip to show year

--- src/sections/overview/analyticis-scatter.tsx
+++ src/sections/overview/analyticis-scatter.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable no-unsafe-optional-chaining */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable arrow-body-style */
 
 // @ts-nocheck
 import { Grid } from '@mui/material';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { ScatterChart } from '@mui/x-charts/ScatterChart';
-
 
 // eslint-disable-next-line react/prop-types
 export default function HomicidesScatterChart({ data }) {
@@ -11,7 +13,17 @@ export default function HomicidesScatterChart({ data }) {
     <Grid item xs={12}>
       <ScatterChart
         height={400}
-        series={[{ label: 'Homicídios x Renda', data }]}
+        dataset={data}
+        // onItemClick={(_: any, d: ScatterItemIdentifier) => alert('d')}
+        series={[
+          {
+            label: (location) => (location === 'tooltip' ? '' : 'Homicídios x Renda'),
+            data,
+            valueFormatter: (value, context) => {
+              return `Ano: ${value.ano} Renda: ${value?.x} Taxa Hom. ${value.y}`;
+            },
+          },
+        ]}
         xAxis={[{ label: 'Renda Per Capita (R$)' }]}
         yAxis={[{ label: 'Taxa de Homicídios (por 100 mil hab.)' }]}
       />
